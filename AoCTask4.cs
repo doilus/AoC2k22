0aa3c7c1902ba520468e2cfeab903e45fd61ba27
doilus <nikalug@onet.eu>
2022-12-04 13:07:30 +0100

    remove bigger logic to other function

--- AoCTask4.cs
+++ AoCTask4.cs
@@ -50,27 +50,8 @@ namespace AocTasks
                 int[] firstPair = pairs[0].Split('-').Select(Int32.Parse).ToArray();
                 int[] secondPair = pairs[1].Split('-').Select(Int32.Parse).ToArray();
 
-                if (firstPair[0] >= secondPair[0] && firstPair[0] <= secondPair[1])
-                {
-                    count++;
-                    continue;
-                }
-                else if (firstPair[1] <= secondPair[1] && firstPair[1] >= secondPair[0])
-                {
-                    count++;
-                    continue;
-                }
-
-                if (secondPair[0] >= firstPair[0] && secondPair[0] <= firstPair[1])
-                {
-                    count++;
-                    continue;
-                }
-                else if (secondPair[1] <= firstPair[1] && secondPair[1] >= firstPair[0])
-                {
-                    count++;
-                    continue;
-                }
+                if (CheckPairs(firstPair, secondPair)) count++;
+                else if (CheckPairs(secondPair, firstPair)) count++;
             }
             Console.WriteLine("Pairs that overlaps: " + count);
         }
@@ -79,6 +60,21 @@ namespace AocTasks
         {
             string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"../../../Inputs/elf_pairs.txt");
             return File.ReadAllLines(path);
+        }
+
+        private bool CheckPairs(int[] pairOne, int[] pairTwo)
+        {
+            if (pairOne[0] >= pairTwo[0] && pairOne[0] <= pairTwo[1])
+            {
+                return true;
+            }
+
+            if (pairOne[1] <= pairTwo[1] && pairOne[1] >= pairTwo[0])
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
