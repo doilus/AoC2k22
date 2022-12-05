81eba29ad002a86c40f5b00109d12dd72d5d4673
doilus <nikalug@onet.eu>
2022-12-05 15:18:02 +0100

    create solution for task 5

---
+++ AoCTask5.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Reflection;
+
+namespace AocTasks
+{
+	public class AoCTask5
+	{
+        public void Resolve()
+        {
+            string lines = ReadFile();
+
+            string[] records = lines.Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<int, List<char>> stacks = PrepareStacks(records[0]);
+
+            string[] moves = records[1].Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string line in moves)
+            {
+                int[] numbers = line.Split(' ').Select(p =>
+                {
+                    var numeric = int.TryParse(p, out int n);
+                    return n;
+                }).ToArray();
+
+                int move = numbers[1];
+                int from = numbers[3];
+                int to = numbers[5];
+
+                var elementsFromStack = stacks[from].Take(move);
+                stacks[to].InsertRange(0, elementsFromStack.Reverse());
+                //stacks[to].InsertRange(0, elementsFromStack); //Uncomment this to have solution for second
+                stacks[from].RemoveRange(0, move);
+            }
+
+            Console.Write("Top of the stack: ");
+
+            for (int i = 1; i <= stacks.Count; i++)
+            {
+                Console.Write(stacks[i].First());
+            }
+        }
+
+        private string ReadFile()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"../../../Inputs/supply_stacks.txt");
+            return File.ReadAllText(path);
+        }
+
+        private Dictionary<int, List<char>> PrepareStacks(string stacksOfCrates)
+        {
+            Dictionary<int, List<char>> stacks =  new Dictionary<int, List<char>>();
+
+            int count = 0;
+            int current = 1;
+
+            foreach (char c in stacksOfCrates)
+            {
+                if (count >= 4)
+                {
+                    count = 0;
+                    current++;
+                }
+
+                if (c.Equals('\n'))
+                {
+                    count = 0;
+                    current = 1;
+                    continue;
+                }
+
+                if (c.Equals('[') || c.Equals(']') || Char.IsWhiteSpace(c))
+                {
+                    count++;
+                    continue;
+                }
+
+
+                if (Char.IsLetter(c))
+                {
+                    if (!stacks.ContainsKey(current)) stacks.Add(current, new List<char>());
+                    stacks[current].Add(c);
+                    count++;
+                }
+                else if (Char.IsNumber(c))
+                {
+                    break;
+                }
+            }
+
+            return stacks;
+        }
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,26 +1,32 @@
 ﻿using System.Reflection;
 using AocTasks;
 
-//Console.WriteLine("Task 1");
-//AoCTask1 task1 = new AoCTask1();
-//task1.Resolve();
+Console.WriteLine("Task 1");
+AoCTask1 task1 = new AoCTask1();
+task1.Resolve();
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Console.WriteLine("Task 2");
-//AoCTask2 task2 = new AoCTask2();
-//task2.Resolve();
+Console.WriteLine("Task 2");
+AoCTask2 task2 = new AoCTask2();
+task2.Resolve();
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Console.WriteLine("Task 3");
-//AoCTask3 task3 = new AoCTask3();
-//task3.ResolveFirst();
-//task3.ResolveSecond();
+Console.WriteLine("Task 3");
+AoCTask3 task3 = new AoCTask3();
+task3.ResolveFirst();
+task3.ResolveSecond();
 
-//Console.WriteLine();
+Console.WriteLine();
 
 Console.WriteLine("Task 4");
 AoCTask4 task4 = new AoCTask4();
 task4.ResolveFirst();
 task4.ResolveSecond();
+
+Console.WriteLine();
+
+Console.WriteLine("Task 5");
+AoCTask5 task5 = new AoCTask5();
+task5.Resolve();
